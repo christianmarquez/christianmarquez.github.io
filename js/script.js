8c360865e5b2e2e7343c7abc286b283874a750ab
christian-gss <christianmarquez.gss@gmail.com>
2018-01-19 22:51:05 +0800

    initial site

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var num = 10; //number of pixels before modifying styles
+
+$(document).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.navbar').addClass('fixed--navbar');
+    } else {
+        $('.navbar').removeClass('fixed--navbar');
+    }
+});
+
+
+  $(document).ready(function() {
+      $('#fullpage').fullpage({
+        scrollOverflow: true
+      });
+    });
+$(document).ready(function($) {
+  $('.menu-icon').on('click', function(e){
+     $('#menu-slide').toggleClass('show');
+  });
+});
+
+jQuery(document).ready(function($) {
+  $('[data-fancybox="group"]').fancybox({
+    thumbs : {
+      autoStart : true
+    },
+    buttons : [
+      'zoom',
+      'close'
+    ]
+  });
+});
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+     $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+    
+  });
+});
